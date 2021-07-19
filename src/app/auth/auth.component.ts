a357a5d9bf837d1dea4105c07dfcd4768188aced
Alexis Dietl <alexisdietl@gmail.com>
2021-07-19 14:51:25 -0300

    FP-0050 LocalStorage

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { IUser } from '../users/user.interface';
@@ -13,7 +13,7 @@ import { ErrorMessage } from '../shared/error-message';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
   formLog: FormGroup = this.fb.group({
     email: [
       '',
@@ -26,6 +26,7 @@ export class AuthComponent implements OnInit {
   });
   loginUserpending$!: Observable<boolean>;
   error!: Subscription;
+  errorDialog: any;
   constructor(
     private fb: FormBuilder,
     private store: Store,
@@ -41,17 +42,22 @@ export class AuthComponent implements OnInit {
     this.loginUserpending$ = this.store.select(loginUserPending);
     this.error = this.store.select(error).subscribe((error) => {
       if (error) {
-        let errorDialog = this.dialog.open(ErrorMessage, {
+        this.errorDialog = this.dialog.open(ErrorMessage, {
           data: { message: error.error },
         });
-        errorDialog.afterClosed().subscribe(() => {
+
+        this.errorDialog.afterClosed().subscribe(() => {
           this.router.navigate(['']);
         });
       }
     });
+    if (this.errorDialog) this.errorDialog.close();
   }
 
   hasError(inputName: 'email' | 'password', errorType: string) {
     return this.formLog.get(inputName)?.hasError(errorType);
+  }
+  ngOnDestroy() {
+    this.error.unsubscribe();
   }
 }
